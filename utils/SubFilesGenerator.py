574d9cee520ac9af743efaf8e70f7e842b117da8
jelenakhlr <108803123+jelenakhlr@users.noreply.github.com>
2023-06-05 13:42:12 +0200

    add runtime specifications

--- utils/SubFilesGenerator.py
+++ utils/SubFilesGenerator.py
@@ -16,12 +16,11 @@ class SubFilesGenerator:
         self.log10_E1 = log10_E1
 
         self.zenith = zenith
-        # TODO: if I do it like this does it know which zenith corresponds to which run number?
 
 
         """
         Generate sub files for Horeka 
-        (by Jelena)
+        (by Jelena) and it is clear <3
 
         """
 
@@ -35,22 +34,41 @@ class SubFilesGenerator:
         # and the .sh file
         sh_file = (f"{self.directory}/{self.log10_E1}/{sim}.sh")
         
+
+        # find theta
+        theta = self.zenith['start']
+
+        # specify runtime according to theta
+        # larger theta require more runtime
+        if theta >= 70:
+            runtime = "04:00:00"
+        elif theta >= 75:
+            runtime = "06:00:00"
+        elif theta >= 77.5:
+            runtime = "10:00:00"
+        elif theta >= 80:
+            runtime = "30:00:00"
+        else:
+            runtime = "03:00:00"
+
+
         # Opening and writing in the file 
         with open(sub_file, "w") as file:
             ######Things that go into the sub file for Horeka#######
             file.write(""
                 + f"#!/bin/bash"
                 + f"#SBATCH --job-name={sim}" # job name is the sim number
-                + f"#SBATCH --output=/home/hk-project-radiohfi/bg5912/work/sims/GRAND/june/logs/_log%a.out"
-                + f"#SBATCH --error=/home/hk-project-radiohfi/bg5912/work/sims/GRAND/june/logs/_log%a.err"
+                + f"#SBATCH --output=/home/hk-project-radiohfi/bg5912/work/sims/GRAND/june/logs/_log%j.out"
+                + f"#SBATCH --error=/home/hk-project-radiohfi/bg5912/work/sims/GRAND/june/logs/_log%j.err"
                 + f"#SBATCH --nodes=2"
                 + f"#SBATCH --cpus-per-task=12"
                 + f"#SBATCH --tasks=2"
+                + f"#SBATCH --time={runtime}"
                 + f""
                 + f"{sh_file}" # run the corresponding sh file, which submits the job to the cluster
             )
 
-# TODO: specify the runtime - maybe according to THETAP?
+
 
     def shWriter(self):
 
@@ -67,7 +85,7 @@ class SubFilesGenerator:
             ######Things that go into the sh file for Horeka#######
             file.write(""
                 + f"#!/bin/bash"
-                + f"sbatch -p cpuonly -A hk-project-radiohfi {sub_file}")
+                + f"sbatch -p cpuonly -A hk-project-radiohfi {sub_file}"
                 # specify the partition you want (e.g. cpuonly)
                 # specify the project you've been assigned to (e.g. radiohfi)
             )
@@ -79,3 +97,4 @@ class SubFilesGenerator:
 
         self.subWriter()
         self.shWriter()
+        
