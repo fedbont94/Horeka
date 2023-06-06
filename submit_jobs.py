cc29abe7cc0f1e22b2af54bc910d81ff8be59acb
jelenakhlr <108803123+jelenakhlr@users.noreply.github.com>
2023-06-06 14:29:38 +0200

    minor changes

--- submit_jobs.py
+++ submit_jobs.py
@@ -9,7 +9,6 @@ import glob
 
 directory = '/home/hk-project-radiohfi/bg5912/work/sims/GRAND/june/sim_storage/inp/'  # Replace with the desired directory path
 sub_files = glob.glob(directory + '/**/*.sub', recursive=True)
-# sub_files = [file for file in os.listdir(directory) if file.endswith('.sub')]
 
 while True:
     if sub_files:
